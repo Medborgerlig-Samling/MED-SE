58c961e0b16c792a257ac0a420b31fbdf17eb7f7
m0satron <18460264+m0satron@users.noreply.github.com>
2025-03-16 10:25:22 +0100

    feat: use Hero component

---
+++ components/Hero/Desktop.vue
@@ -0,0 +1,14 @@
+<template>
+<v-parallax :src="image">
+      <div class="d-flex flex-column fill-height justify-center align-center text-white">
+        <h1 class="text-h4 font-weight-thin mb-4">{{ title }} </h1>
+        <h4 class="subheading">{{ subtitle  }}</h4>
+        <v-btn :to="CTA?.to" variant="text" color="white" append-icon="mdi-arrow-right"> {{  CTA?.title }} </v-btn>
+      </div>
+    </v-parallax>
+</template>
+
+<script setup lang="ts">
+import type { Hero } from '~/types/layout/Hero';
+defineProps<Hero>();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,23 @@
 <template>
   <div>
-    <v-parallax :src="heroImg">
-      <div class="d-flex flex-column fill-height justify-center align-center text-white">
-        <h1 class="text-h4 font-weight-thin mb-4">Det är dags för en ny svensk borgerlighet</h1>
-        <h4 class="subheading">Sverige behöver en omstart - Vi är beredda att göra jobbet</h4>
-        <v-btn to="/medlem" variant="text" color="white" append-icon="mdi-arrow-right">Läs mer</v-btn>
-      </div>
-    </v-parallax>
-
+  <HeroDesktop v-if="(hero && hero.title && hero.CTA && hero.image)" v-bind="hero"/>
     <v-container>
-      <h2 class="text-h5 text-center mb-4">Detta vill vi</h2>
-
-      <v-card max-width="344">
-        <v-img height="200px" :src="cardOneImage" cover />
-        <v-card-title>Stoppa skatteslöseriet</v-card-title>
-        <v-card-subtitle> Skattepengar ska gå till kärnverksamhet. </v-card-subtitle>
-        <v-card-actions>
-          <v-spacer />
-
-          <v-btn :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show = !show" />
-        </v-card-actions>
-
-        <v-expand-transition>
-          <div v-show="show">
-            <v-divider />
-
-            <v-card-text>
-              Vad får vi egentligen för pengarna? Åratal av politisk misskötsel har lett fram till ett bidragssamhälle
-              som prioriterar oväsentligheter framför statens kärnverksamhet. Det är en myt att höga skatter och hög
-              välfärd måste hänga ihop. Vi kan sänka skatterna med upp till 20% och ändå ge mer pengar till skola,
-              rättsväsende, försvar och vård.
-            </v-card-text>
-          </div>
-        </v-expand-transition>
-      </v-card>
-      <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer /> <v-spacer />
       <v-spacer />
     </v-container>
   </div>
 </template>
 
-<script setup>
-const runtimeConfig = useRuntimeConfig();
-const baseURL = runtimeConfig.public.baseURL;
-const heroImg = ref(''); // Store the image URL
+<script setup lang="ts">
+import type { Hero } from '~/types/layout/Hero';
+const hero = ref<Hero | null>(null)
 
-const show = ref(false);
-
-const cardOneImage = ref(''); // Store the image URL
 
 onMounted(async () => {
   try {
-    heroImg.value = `${baseURL}/uploads/sunrise_over_sweden_volumetric_light_where_the_su_9_5f0a560f6a.jpg`; // Set the correct URL
-    cardOneImage.value = `${baseURL}/uploads/sloseri_055b1cad43.jpg`; // Set the correct URL
+    const res = await fetch('/api/home');
+    const json = await res.json()
+    hero.value = json.hero
+
   } catch (error) {
     console.error('Error fetching image:', error);
   }

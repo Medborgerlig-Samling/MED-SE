478561682071969bff573c64b1cc525d5ee79360
m0satron <18460264+m0satron@users.noreply.github.com>
2025-03-21 09:19:10 +0100

    feat: add heros

--- components/Hero/Desktop.vue
+++ components/Hero/Desktop.vue
@@ -1,11 +1,20 @@
 <template>
-<v-parallax :src="image  ||  'hero_placeholder.jpg'" rounded>
-      <div class="d-flex flex-column fill-height justify-center align-center text-white">
+  <v-parallax
+    :src="image || 'hero_placeholder.jpg'"
+    rounded
+    gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
+  >
+    <!-- <div class="d-flex flex-column fill-height justify-center align-center text-white">
         <h1 class="text-h4 font-weight-thin mb-4">{{ title }} </h1>
         <h4 class="subheading">{{ subtitle  }}</h4>
         <v-btn v-if="CTA" :to="CTA?.to" variant="text" color="white" append-icon="mdi-arrow-right"> {{  CTA?.title }} </v-btn>
-      </div>
-    </v-parallax>
+      </div> -->
+    <div class="d-flex flex-column fill-height pa-6 justify-center align-center text-white">
+      <h1 class="text-h2 font-weight-black text-center text-break mb-4">{{ title }}</h1>
+      <h4 class="text-h5 text-center font-weight-bold mb-4">{{ subtitle }}</h4>
+      <v-btn v-if="CTA" :to="CTA?.to" color="accent" flat append-icon="mdi-arrow-right"> {{ CTA?.title }} </v-btn>
+    </div></v-parallax
+  >
 </template>
 
 <script setup lang="ts">
--- components/Hero/MemberDesktop.vue
+++ components/Hero/MemberDesktop.vue
@@ -1,59 +1,49 @@
 <template>
-  <v-parallax :src="image  ||  'hero_placeholder.jpg'">
+  <v-parallax :src="image || 'hero_placeholder.jpg'" rounded>
     <div class="wrapper">
-        <v-sheet 
-
-        color="transparent"
-          class="d-flex text-white flex-column text-primary pa-6">
-          <h4 class="text-h5 font-weight-thin mb-4 bg-accent title pa-2">{{ title }} </h4>
-          <h1 class="text-h1 name mb-4">{{ firstName }} </h1>
-          <h1 class="text-h1 name mb-4">{{ familyName }} </h1>
-          <h2 class="subheading font-italic">{{ slogan  }}</h2>
-          <!-- <v-avatar :size="300" class="elevation-12 mx-auto mb-8">
+      <v-sheet color="transparent" class="d-flex text-white flex-column text-primary pa-6">
+        <h4 class="text-h5 font-weight-thin mb-4 bg-accent title pa-2">{{ title }}</h4>
+        <h1 class="text-h1 name mb-4">{{ firstName }}</h1>
+        <h1 class="text-h1 name mb-4">{{ familyName }}</h1>
+        <h2 class="subheading font-italic">{{ slogan }}</h2>
+        <!-- <v-avatar :size="300" class="elevation-12 mx-auto mb-8">
             <v-img :src="portrait"/>
           </v-avatar> -->
-          <div class="d-flex gap-2 mt-16">
-
+        <div class="d-flex gap-2 mt-16">
           <v-btn
             v-if="twitter"
             class="mr-3 mt-5"
             icon="mdi-twitter"
             color="white"
-                  variant="outlined"
+            variant="outlined"
             :href="`https://www.x.com/${selectedMember?.twitter}`"
             target="_blank"
           />
-          <v-btn 
-            class="mr-3 mt-5" 
-            color="white"
-            variant="outlined"
-            icon="mdi-email" 
-            :href="`mailto:${email}`" 
-          />
-          
-          </div>
-        </v-sheet>
+          <v-btn class="mr-3 mt-5" color="white" variant="outlined" icon="mdi-email" :href="`mailto:${email}`" />
+        </div>
+      </v-sheet>
     </div>
   </v-parallax>
-  </template>
-  
-  <script setup lang="ts">
-  import type { Hero } from '~/types/layout/Hero';
-  defineProps<Hero & { firstName: string; familyName: string; slogan: string; twitter: string; email: string; portrait: string}>();
-  </script>
+</template>
 
-  <style scoped>
-  
-  .name {
-    font-weight: bold!important;
-  }
+<script setup lang="ts">
+import type { Hero } from '~/types/layout/Hero';
+defineProps<
+  Hero & { firstName: string; familyName: string; slogan: string; twitter: string; email: string; portrait: string }
+>();
+</script>
 
-  .title {
-    width: fit-content;
-  }
+<style scoped>
+.name {
+  font-weight: bold !important;
+}
 
-  .action {
-    position: absolute;
-    right: 0;
-  }
-  </style>
+.title {
+  width: fit-content;
+}
+
+.action {
+  position: absolute;
+  right: 0;
+}
+</style>

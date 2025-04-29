353a7014d0c672b08ccf0c8b4abb3d092cf21323
m0satron <18460264+m0satron@users.noreply.github.com>
2025-04-29 22:10:54 +0200

    feat: add items to home page

--- server/api/transformers/homeTransformer.ts
+++ server/api/transformers/homeTransformer.ts
@@ -1,21 +1,53 @@
 interface HomeData {
   slug: string;
+  corevalues: {
+    title: string;
+    slug: string;
+    subtitle: string;
+    images: { url: string; formats: { small: { url: string } } }[];
+  }[];
+  news_items: {
+    caption: string;
+    url: string;
+    source: string;
+    tags: { label: string; value: string }[];
+    image: { url: string; formats: { small: { url: string } } }[];
+  };
   hero: {
     title: string;
     subtitle: string;
     image: { url: string }[];
+
     CTA: {
       label: string;
       value: string;
     };
   };
 }
-
 export function transformHomeData({ data, baseUrl }: { data: HomeData; baseUrl: string }) {
-  const { slug, hero } = data;
+  const { slug, hero, news_items, corevalues } = data;
 
   return {
     slug,
+    seo: useSeoParser(data),
+    coreValues:
+      corevalues?.map((coreValue) => ({
+        title: coreValue.title,
+        slug: coreValue.slug,
+        subtitle: coreValue.subtitle,
+        image: coreValue.images[0].formats.small.url,
+      })) || [],
+    newsItems:
+      news_items?.map((newsItem) => ({
+        caption: newsItem.caption,
+        url: newsItem.url,
+        source: newsItem.source,
+        tags: newsItem.tags.map((tag) => ({
+          label: tag.label,
+          value: tag.value,
+        })),
+        image: newsItem.image?.formats?.small?.url,
+      })) || [],
     hero: {
       title: hero.title,
       subtitle: hero.subtitle,
